01a428c9d2f00ccdc63bb1bba98a39fb28542f5a
armin <a@b.c>
2026-07-31 22:49:13 +0300

    data elements better positioning

--- src/layout/try_move_nodes_into_half_layer.rs
+++ src/layout/try_move_nodes_into_half_layer.rs
@@ -1,6 +1,8 @@
-use crate::common::graph::{DATAOBJECT_NODE_HEIGHT, Graph};
-use crate::common::node::LayerId;
-use proc_macros::to;
+use crate::common::edge::FlowType;
+use crate::common::graph::{DATAOBJECT_NODE_HEIGHT, EdgeId, Graph, NodeId};
+use crate::common::index_iter::IterIndices;
+use crate::common::node::{LayerId, Node};
+use proc_macros::{from, n, to};
 use std::collections::HashMap;
 use std::ops::RangeInclusive;
 
@@ -28,67 +30,167 @@ pub fn try_move_nodes_into_half_layer(graph: &mut Graph) {
 
     // Go through the nodes on regular layers.
     // `left` part of the edges we inspect right now.
-    for left_node in &graph.nodes {
-        if left_node.uses_half_layer {
+    for (edge_idx, edge) in graph.edges.iter().enumerate() {
+        if edge.is_replaced_by_dummies() {
             continue;
         }
-
-        // TODO edges of data nodes are currently ignored, meaning that we accept that data nodes
-        // are placed in a halflayer even if there is a data edge crossing it. Thing is, there is
-        // some complication here: data edges might be drawn straight altogether, in which case
-        // the current crossing might disappear. So ignore it for now. In the future one should
-        // record whether the *only* vertical edge segment conflicts come from data edges and then
-        // check whether they have been replaced with straight lines. So it's a bit complicated
-        // and needs experiments and further thinking. Right now it is ignored, as it should
-        // (hopefully!) be an edge case anyway.
-        if left_node.is_data() {
-            continue;
-        }
-
-        // Approximately correct `y` value. It just needs to be *some* `y` value across the sides of
-        // the node. Could also use the port.
-        let start_top = left_node.y;
-        let start_bottom = left_node.y + left_node.height;
-        let vertical_segments: &mut Vec<_> = vertical_segments_per_layer
-            .entry(left_node.layer_id)
-            .or_default();
-        for outgoing_edge_id in &left_node.outgoing {
-            let to_node = &to!(*outgoing_edge_id);
-
-            // Similar to the `left_node.is_data()` check. In the case where to_node is
-            // actually just in the next half-layer, then we need to not record this edge here,
-            // as otherwise the incoming data edge blocks the to_node (a data node) from
-            // being put into the half-layer.
-            if to_node.is_data() && to_node.layer_id == left_node.layer_id {
+        let edge_id = EdgeId(edge_idx);
+        let (left_node, right_node) = (&n!(edge.from), &n!(edge.to));
+        match edge.flow_type {
+            FlowType::DataFlow(..) => {
+                // TODO edges of data nodes are currently ignored, meaning that we accept that data nodes
+                // are placed in a halflayer even if there is a data edge crossing it. Thing is, there is
+                // some complication here: data edges might be drawn straight altogether, in which case
+                // the current crossing might disappear. So ignore it for now. In the future one should
+                // record whether the *only* vertical edge segment conflicts come from data edges and then
+                // check whether they have been replaced with straight lines. So it's a bit complicated
+                // and needs experiments and further thinking. Right now it is ignored, as it should
+                // (hopefully!) be an edge case anyway.
                 continue;
             }
-
-            let end_top = to_node.y;
-            let end_bottom = to_node.y + to_node.height;
-            vertical_segments.push(
-                start_top.min(end_top).saturating_sub(top_padding)
-                    ..=start_bottom.max(end_bottom).saturating_add(bottom_padding),
-            );
+            FlowType::MessageFlow(..) => {
+                let pool = &graph.pools[left_node.pool];
+                // The midpoint is enforced by edge routing as well. It is an invariant of this tool
+                // that message flows go horizontal (if necessary) in the very first inter-pool space,
+                // i.e. after leaving the starting pool.
+                let midpoint = if left_node.pool_and_lane() < right_node.pool_and_lane() {
+                    pool.y + pool.height
+                } else {
+                    pool.y
+                };
+                'first: {
+                    let port_of_outgoing = left_node.port_of_outgoing(edge_id);
+                    let layer_id = if left_node.is_any_dummy() {
+                        // This is a bend dummy, where the flow leaves the node at the top or
+                        // bottom, but then must route around another node obstacle. So it will go
+                        // into the next layer.
+                        left_node.layer_id
+                    } else {
+                        if !left_node.port_is_left_or_right(port_of_outgoing.y) {
+                            break 'first;
+                        }
+                        if port_of_outgoing.x == left_node.x {
+                            if left_node.layer_id.0 == 0 {
+                                // This is the left-most layer. We don't move data elements into the -0.5
+                                // half layer (well, maybe we actually should? not sure how often this situation
+                                // occurs that there is a data object coming in from nowhere at the start).
+                                break 'first;
+                            }
+                            LayerId(left_node.layer_id.0.strict_sub(1))
+                        } else {
+                            left_node.layer_id
+                        }
+                    };
+                    let vertical_segments =
+                        vertical_segments_per_layer.entry(layer_id).or_default();
+                    vertical_segments.push(
+                        left_node.y.min(midpoint).saturating_sub(top_padding)
+                            ..=(left_node.y + left_node.height)
+                                .max(midpoint)
+                                .saturating_add(bottom_padding),
+                    );
+                }
+                'last: {
+                    let port_of_incoming = right_node.port_of_incoming(edge_id);
+                    let layer_id = if right_node.is_any_dummy() {
+                        // This is a bend dummy, where the flow enters the node at the top or
+                        // bottom, but to do so must route around another node obstacle. So it will
+                        // come in from the left layer.
+                        if right_node.layer_id.0 == 0 {
+                            // This is the left-most layer. We don't move data elements into the -0.5
+                            // half layer (well, maybe we actually should? not sure how often this situation
+                            // occurs that there is a data object coming in from nowhere at the start).
+                            break 'last;
+                        }
+                        LayerId(right_node.layer_id.0.strict_sub(1))
+                    } else {
+                        if !right_node.port_is_left_or_right(port_of_incoming.y) {
+                            break 'last;
+                        }
+                        if port_of_incoming.x == right_node.x {
+                            if right_node.layer_id.0 == 0 {
+                                // This is the left-most layer. We don't move data elements into the -0.5
+                                // half layer (well, maybe we actually should? not sure how often this situation
+                                // occurs that there is a data object coming in from nowhere at the start).
+                                break 'last;
+                            }
+                            LayerId(right_node.layer_id.0.strict_sub(1))
+                        } else {
+                            right_node.layer_id
+                        }
+                    };
+                    let vertical_segments =
+                        vertical_segments_per_layer.entry(layer_id).or_default();
+                    vertical_segments.push(
+                        right_node.y.min(midpoint).saturating_sub(top_padding)
+                            ..=(right_node.y + right_node.height)
+                                .max(midpoint)
+                                .saturating_add(bottom_padding),
+                    );
+                }
+                // Segments are added, the rest of the logic is for the case where we have a sequence flow.
+            }
+            FlowType::SequenceFlow => {
+                if edge.is_vertical {
+                    // This does not block the half-layer space.
+                    continue;
+                }
+                // Approximately correct `y` value. It just needs to be *some* `y` value across the sides of
+                // the node. Could also use the port.
+                let start_top = left_node.y;
+                let start_bottom = left_node.y + left_node.height;
+                let vertical_segments = vertical_segments_per_layer
+                    .entry(left_node.layer_id)
+                    .or_default();
+                let end_top = right_node.y;
+                let end_bottom = right_node.y + right_node.height;
+                vertical_segments.push(
+                    start_top.min(end_top).saturating_sub(top_padding)
+                        ..=start_bottom.max(end_bottom).saturating_add(bottom_padding),
+                );
+            }
         }
     }
 
-    'outer: for node in &mut graph.nodes {
-        if !node.uses_half_layer {
+    'outer: for node_id in graph.nodes.len().iter_indices(false).map(NodeId) {
+        if !n!(node_id).uses_half_layer {
             continue;
         }
 
         let vertical_segments: &Vec<_> = vertical_segments_per_layer
-            .entry(node.layer_id)
+            .entry(n!(node_id).layer_id)
             .or_default();
 
         for vertical_segment in vertical_segments.iter() {
-            if vertical_segment.contains(&node.y) {
-                node.uses_half_layer = false;
+            if vertical_segment.contains(&n!(node_id).y) {
+                n!(node_id).uses_half_layer = false;
                 continue 'outer;
             }
         }
 
         // No conflict, so shift it right.
-        node.x += graph.config.layer_width() / 2;
+        if let [incoming] = &n!(node_id).incoming[..]
+            && let [outgoing] = &n!(node_id).outgoing[..]
+            && (data_element_sandwiched(&from!(*incoming), &n!(node_id), &to!(*outgoing))
+                || data_element_sandwiched(&from!(*incoming), &n!(node_id), &to!(*outgoing)))
+        {
+            let from = from!(*incoming).dimension();
+            let to = to!(*outgoing).dimension();
+            let node_width = n!(node_id).width;
+            if from.x < to.x {
+                n!(node_id).x = (from.x + from.width).midpoint(to.x) - node_width / 2;
+            } else {
+                n!(node_id).x = (to.x + to.width).midpoint(from.x) - node_width / 2;
+            }
+        } else {
+            n!(node_id).x += graph.config.layer_width() / 2;
+        }
     }
+}
+
+fn data_element_sandwiched(left: &Node, data_node: &Node, right: &Node) -> bool {
+    left.pool_and_lane() == data_node.pool_and_lane()
+        && left.layer_id == data_node.layer_id
+        && right.pool_and_lane() == data_node.pool_and_lane()
+        && data_node.layer_id.0 + 1 == right.layer_id.0
 }
